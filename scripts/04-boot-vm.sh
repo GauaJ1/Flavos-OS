830eae956a64a8276c80d53e0f116f863292aed1
GauaJ1 <kaua31027@gmail.com>
2026-04-05 16:40:21 -0300

    fix(qemu): Fixa resolucao da VM GUI em 1024x768

--- scripts/04-boot-vm.sh
+++ scripts/04-boot-vm.sh
@@ -86,9 +86,9 @@ case "$DISPLAY_MODE" in
         echo "Display:   serial (texto)"
         ;;
     --gui)
-        DISPLAY_FLAGS="-display gtk -serial stdio"
-        INPUT_FLAGS="-device usb-ehci,id=ehci -device usb-tablet,bus=ehci.0"
-        echo "Display:   GUI (GTK) com usb-tablet"
+        DISPLAY_FLAGS="-display gtk -serial stdio -vga std"
+        INPUT_FLAGS="-device usb-ehci,id=ehci -device usb-tablet,bus=ehci.0 -device VGA,xres=1024,yres=768"
+        echo "Display:   GUI (GTK) 1024x768 com usb-tablet"
         ;;
 esac
 
